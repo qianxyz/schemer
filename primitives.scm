3a87ab5756da14dabfca854fbeb05df4fa6a360f
Xinyuan Qian <xyqian67@gmail.com>
2026-08-21 00:23:32 -0400

    Add Scheme source for primitive procedures

---
+++ primitives.scm
@@ -0,0 +1,58 @@
+(quote atom)
+(quote 1942)
+(quote (atom))
+(quote (atom turkey or))
+(quote ((atom turkey) or))
+(quote ())
+(quote (() () () ()))
+
+(car (quote (a b c)))
+(car (quote ((a b c) x y z)))
+(car (quote hotdog))
+(car (quote ()))
+(car (quote (((hotdogs)) (and) (pickle) relish)))
+(car (car (quote (((hotdogs)) (and)))))
+
+(cdr (quote (a b c)))
+(cdr (quote ((a b c) x y z)))
+(cdr (quote (hamburger)))
+(cdr (quote ((x) t r)))
+(cdr (quote hotdogs))
+(cdr (quote ()))
+
+(car (cdr (quote ((b) (x y) ((c))))))
+(cdr (cdr (quote ((b) (x y) ((c))))))
+(cdr (car (quote (a (b (c)) d))))
+
+(cons (quote peanut) (quote (butter and jelly)))
+(cons (quote (banana and)) (quote (peanut butter and jelly)))
+(cons (quote ((help) this)) (quote (is very ((hard) to learn))))
+(cons (quote (a b (c))) (quote ()))
+(cons (quote ((a b c))) (quote b))
+(cons (quote a) (quote b))
+
+(cons (quote a) (car (quote ((b) c d))))
+(cons (quote a) (cdr (quote ((b) c d))))
+
+(null? (quote ()))
+(null? (quote (a b c)))
+(null? (quote spaghetti))
+
+(atom? (quote Harry))
+(atom? (quote (Harry had a heap of apples)))
+(atom? (car (quote (Harry had a heap of apples))))
+(atom? (cdr (quote (Harry had a heap of apples))))
+(atom? (cdr (quote (Harry))))
+(atom? (car (cdr (quote (swing low sweet cherry oat)))))
+(atom? (car (cdr (quote (swing (low sweet) cherry oat)))))
+
+(eq? (quote Harry) (quote Harry))
+(eq? (quote margarine) (quote butter))
+(eq? (quote ()) (quote (strawberry)))
+(eq? 6 7)
+
+(eq? (car (quote (Mary had a little lamb chop))) (quote Mary))
+(eq? (cdr (quote (soured milk))) (quote milk))
+
+(define l (quote (beans beans we need jelly beans)))
+(eq? (car l) (car (cdr l)))
