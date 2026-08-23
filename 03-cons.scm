59c1e5aae0a68c4f56911035f856ed4525ef785c
Xinyuan Qian <xyqian67@gmail.com>
2026-08-22 23:19:58 -0400

    Rename scm files by book chapter

--- 03-cons.scm
+++ 03-cons.scm
@@ -1,24 +1,3 @@
-(define lat? ; list of atoms
-  (lambda (l)
-    (cond
-      ((null? l) #t)
-      ((atom? (car l)) (lat? (cdr l)))
-      (else #f))))
-
-(lat? (quote ()))
-(lat? (quote (bacon and eggs)))
-(lat? (quote (bacon (and eggs))))
-
-(define member?
-  (lambda (a lat)
-    (cond
-      ((null? lat) #f)
-      (else (or (eq? a (car lat)) 
-                (member? a (cdr lat)))))))
-
-(member? (quote tea) (quote (coffee tea or milk)))
-(member? (quote poached) (quote (fried eggs and scrambled eggs)))
-
 (define rember ; remove member
   (lambda (a lat)
     (cond
