2500917a26d500d4ab48c834ab97e4da4bb28e9b
Xinyuan Qian <xyqian67@gmail.com>
2026-09-23 22:34:42 -0700

    Implement happy path of numeric operations

--- src/Schemer/Eval.hs
+++ src/Schemer/Eval.hs
@@ -1,5 +1,6 @@
 module Schemer.Eval where
 
+import Data.Maybe (fromMaybe)
 import Schemer.Types
 
 eval :: SExp -> SExp
@@ -8,7 +9,41 @@ eval e@(String _) = e
 eval e@(Bool _) = e
 eval e@(Char _) = e
 eval (List [Atom "quote", e]) = e
+eval (List (Atom func : args)) = apply func $ map eval args
 eval (Atom _) = undefined -- TODO
 eval (List _) = undefined -- TODO
 eval (DottedList _ _) = undefined -- TODO
 eval (Vector _) = undefined -- TODO
+
+apply :: String -> [SExp] -> SExp
+apply func args = maybe (undefined) ($ args) $ lookup func primitives
+
+primitives :: [(String, [SExp] -> SExp)]
+primitives =
+  [ ("+", primPlusMult (+) 0),
+    ("-", primMinusDiv (-) 0),
+    ("*", primPlusMult (*) 1),
+    ("/", primMinusDiv (unsafeDiv) 1),
+    ("modulo", Number . Int . uncurry mod . assertIntPair),
+    ("quotient", Number . Int . uncurry quot . assertIntPair),
+    ("remainder", Number . Int . uncurry rem . assertIntPair)
+  ]
+
+unsafeDiv :: Number -> Number -> Number
+unsafeDiv a b = fromMaybe undefined $ safeDiv a b
+
+assertNumber :: SExp -> Number
+assertNumber (Number n) = n
+assertNumber _ = undefined
+
+assertIntPair :: [SExp] -> (Integer, Integer)
+assertIntPair [Number (Int n), Number (Int m)] = (n, m)
+assertIntPair _ = undefined
+
+primPlusMult :: (Number -> Number -> Number) -> Number -> [SExp] -> SExp
+primPlusMult op zero = Number . foldl op zero . map assertNumber
+
+primMinusDiv :: (Number -> Number -> Number) -> Number -> [SExp] -> SExp
+primMinusDiv _ _ [] = undefined
+primMinusDiv op zero [e] = Number . op zero $ assertNumber e
+primMinusDiv op _ exprs = Number . foldl1 op $ map assertNumber exprs
---
+++ test/Schemer/EvalSpec.hs
@@ -0,0 +1,101 @@
+module Schemer.EvalSpec (spec) where
+
+import Data.Complex (Complex ((:+)))
+import Data.Ratio ((%))
+import Schemer.Eval (eval)
+import Schemer.Parse (readExpr)
+import Schemer.Types
+import Test.Hspec
+
+-- | Parse a string and evaluate the resulting expression.
+evalString :: String -> Either String SExp
+evalString source = eval <$> readExpr source
+
+-- | Assert that a source string evaluates to the given number.
+evaluatesTo :: String -> Number -> Expectation
+evaluatesTo source n = evalString source `shouldBe` Right (Number n)
+
+infix 1 `evaluatesTo`
+
+spec :: Spec
+spec = do
+  describe "self-evaluating expressions" $ do
+    it "evaluates a number to itself" $
+      "42" `evaluatesTo` Int 42
+    it "evaluates a string to itself" $
+      evalString "\"abc\"" `shouldBe` Right (String "abc")
+    it "evaluates a boolean to itself" $
+      evalString "#t" `shouldBe` Right (Bool True)
+    it "evaluates a character to itself" $
+      evalString "#\\a" `shouldBe` Right (Char 'a')
+
+  describe "quote" $ do
+    it "returns a quoted atom unevaluated" $
+      evalString "(quote a)" `shouldBe` Right (Atom "a")
+    it "returns a quoted list unevaluated" $
+      evalString "(quote (+ 1 2))"
+        `shouldBe` Right (List [Atom "+", Number (Int 1), Number (Int 2)])
+    it "treats the ' prefix the same as quote" $
+      evalString "'(a b)" `shouldBe` Right (List [Atom "a", Atom "b"])
+
+  describe "+" $ do
+    it "returns 0 with no arguments" $
+      "(+)" `evaluatesTo` Int 0
+    it "returns its single argument" $
+      "(+ 5)" `evaluatesTo` Int 5
+    it "adds several integers" $
+      "(+ 1 2 3)" `evaluatesTo` Int 6
+    it "demotes an exact rational sum to an integer" $
+      "(+ 1/2 1/2)" `evaluatesTo` Int 1
+    it "becomes inexact when any argument is inexact" $
+      "(+ 1 2.5)" `evaluatesTo` Float 3.5
+    it "demotes a complex sum with a zero imaginary part" $
+      "(+ 1+2i 3-2i)" `evaluatesTo` Int 4
+
+  describe "-" $ do
+    it "negates a single argument" $
+      "(- 5)" `evaluatesTo` Int (-5)
+    it "negates a single rational" $
+      "(- 1/2)" `evaluatesTo` Rational ((-1) % 2)
+    it "subtracts from left to right" $
+      "(- 10 1 2)" `evaluatesTo` Int 7
+
+  describe "*" $ do
+    it "returns 1 with no arguments" $
+      "(*)" `evaluatesTo` Int 1
+    it "multiplies several integers" $
+      "(* 2 3 4)" `evaluatesTo` Int 24
+    it "becomes inexact when any argument is inexact" $
+      "(* 2 0.5)" `evaluatesTo` Float 1.0
+    it "demotes a complex product with a zero imaginary part" $
+      "(* 1+i 1-i)" `evaluatesTo` Int 2
+
+  describe "/" $ do
+    it "takes the reciprocal of a single argument" $
+      "(/ 2)" `evaluatesTo` Rational (1 % 2)
+    it "demotes an exact quotient to an integer" $
+      "(/ 6 3)" `evaluatesTo` Int 2
+    it "keeps a non-integer exact quotient as a rational" $
+      "(/ 1 3)" `evaluatesTo` Rational (1 % 3)
+    it "divides from left to right" $
+      "(/ 12 2 3)" `evaluatesTo` Int 2
+    it "becomes inexact when any argument is inexact" $
+      "(/ 1 2.0)" `evaluatesTo` Float 0.5
+    it "divides exact complex numbers exactly" $
+      "(/ 1+2i 3+4i)" `evaluatesTo` Complex (RRational (11 % 25) :+ RRational (2 % 25))
+
+  describe "integer division" $ do
+    it "quotient truncates toward zero" $
+      "(quotient -7 2)" `evaluatesTo` Int (-3)
+    it "remainder takes the sign of the dividend" $
+      "(remainder -7 2)" `evaluatesTo` Int (-1)
+    it "modulo takes the sign of the divisor" $
+      "(modulo -7 2)" `evaluatesTo` Int 1
+    it "modulo with a negative divisor" $
+      "(modulo 7 -2)" `evaluatesTo` Int (-1)
+
+  describe "nested expressions" $ do
+    it "evaluates arguments before applying" $
+      "(+ 1 (* 2 3))" `evaluatesTo` Int 7
+    it "evaluates deeply nested arithmetic" $
+      "(/ (- 10 (* 2 3)) (+ 1 1))" `evaluatesTo` Int 2
